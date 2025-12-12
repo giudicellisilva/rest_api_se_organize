a305c7f17027848f5ddf4c580e79042051176156
Giudicelli Elias <86128363+giudicellisilva@users.noreply.github.com>
2025-12-12 11:37:26 -0300

    first commit

---
+++ src/main/java/silva/giudicelli/rest_api_se_organize/RestApiSeOrganizeApplication.java
@@ -0,0 +1,13 @@
+package silva.giudicelli.rest_api_se_organize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSeOrganizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiSeOrganizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/silva/giudicelli/rest_api_se_organize/RestApiSeOrganizeApplicationTests.java
@@ -0,0 +1,13 @@
+package silva.giudicelli.rest_api_se_organize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSeOrganizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
